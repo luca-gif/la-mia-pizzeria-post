e7a0e34c12b8989a76e987ba4b207e8fcaf79a6b
luca-gif <luca.89m@gmail.com>
2022-10-05 12:48:05 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,53 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(Pizzas());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            
+            return View(Pizzas()[id]);
+        }
+
+        public List<Pizza> Pizzas()
+        {
+            List<Pizza> pizze = new List<Pizza> {
+
+                new Pizza(0, "Margherita", "Pomodoro, Mozzarella", "/img/pizza-margherita.jpg", 5),
+                new Pizza(1, "Diavola", "Pomodoro, Mozzarella, salame piccante", "/img/pizza-diavola.jpg", 7),
+                new Pizza(2, "Marinara", "Pomodoro", "/img/pizza-marinara.jpg", 4),
+                new Pizza(3, "Salsiccia", "Pomodoro, Mozzarella, salsiccia", "/img/pizza-salsiccia.jpg", 7),
+
+            };
+
+            return pizze;
+        }
+
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,21 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(int id, string name, string description, string image, double price)
+        {
+            PizzaId = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+    }
+}
